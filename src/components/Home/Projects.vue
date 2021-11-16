799656e912c6e2dc049b8c914382d27f666a1276
EdixonAlberto <edixonalbertto@gmail.com>
2021-11-15 23:57:05 -0400

    improve styles and change of css to scss in projects

--- src/components/Home/Projects.vue
+++ src/components/Home/Projects.vue
@@ -1,29 +1,29 @@
 <template>
   <div class="projects">
-    <ul class="projects__container">
-      <li v-for="(project, i) in projects" :key="i" class="projects__card">
-        <div v-if="project.title">
-          <div class="projects__banner" :style="{ backgroundImage: `url('${project.image}')` }"></div>
+    <ul class="container">
+      <li v-for="(project, i) in projects" :key="i" class="card">
+        <div class="layer"></div>
 
-          <div class="projects__content">
-            <h2 class="mb-2">{{ project.title }}</h2>
-            <p class="mb-2">{{ project.description }}</p>
+        <div class="banner" :style="{ backgroundImage: `url('${project.image}')` }"></div>
 
-            <h3>Enlaces:</h3>
+        <div class="content">
+          <h2>{{ project.title }}</h2>
+          <p class="mb-2">{{ project.description }}</p>
 
-            <ul>
-              <li v-for="(link, i) in project.links" :key="i">
-                <g-link :to="link" target="_blank"> 🔗 {{ link }} </g-link>
-              </li>
-            </ul>
+          <h3>Enlaces:</h3>
 
-            <h3 class="mt-3">
-              Estado:
-              <div class="badge-state" :class="{ completed: project.completed }">
-                <span class="text-white font-semibold" v-text="project.completed ? 'Completed' : 'In Progress'"></span>
-              </div>
-            </h3>
-          </div>
+          <ul>
+            <li v-for="(link, i) in project.links" :key="i" class="mb-1">
+              <g-link :to="link" target="_blank">🔗 {{ link }}</g-link>
+            </li>
+          </ul>
+
+          <h3 class="mt-2">
+            Estado:
+            <div class="badge-state" :class="{ completed: project.completed }">
+              <span class="text-white font-semibold" v-text="project.completed ? 'Completed' : 'In Progress'"></span>
+            </div>
+          </h3>
         </div>
       </li>
     </ul>
@@ -45,103 +45,138 @@ export default Vue.extend({
 });
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+$height-card: 470px;
+
 .projects {
-  --height-card: 450px;
-  width: 100%;
-}
-
-.projects .projects__container {
-  width: 1200px;
-  padding: 20px;
-  margin-left: -100px;
-
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-template-rows: auto;
-  gap: 10px;
-
-  justify-content: center;
-  align-items: flex-start;
-
-  @apply bg-background-tertiary;
-  border: none;
-  border-radius: 20px;
-}
-
-.projects__card {
-  height: var(--height-card);
-  width: 100%;
-  position: relative;
-
-  display: flex;
-  flex-direction: column;
-  row-gap: 10px;
-
-  @apply bg-background-primary;
-  border-radius: 30px;
-
-  transform: scale(0.95);
-  transition: transform 0.35s ease-in-out;
-}
-
-.projects__card:hover {
-  transform: scale(1);
-}
-
-.projects__banner {
-  height: calc(var(--height-card) * 0.4);
   width: 100%;
 
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: top;
-  border-radius: 30px 30px 0 0;
-}
+  .container {
+    width: 1200px;
+    margin-left: -100px;
+    padding: 20px;
 
-.projects__content {
-  height: calc(var(--height-card) * 0.6);
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-end;
-  padding: 20px;
-}
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: auto;
+    gap: 10px;
 
-.projects__content h2 {
-  font-size: 24px;
-  font-weight: 700;
-  margin-bottom: 12px;
-}
+    justify-content: center;
+    align-items: flex-start;
 
-.projects__content p,
-span {
-  font-size: 18px;
-}
+    @apply bg-background-tertiary;
+    border: none;
+    border-radius: 20px;
 
-.projects__content li {
-  font-size: 16px;
-}
+    .card {
+      height: $height-card;
+      width: 100%;
+      position: relative;
+      overflow: hidden;
 
-.projects-img {
-  min-height: 15rem;
-}
+      display: flex;
+      flex-direction: column;
 
-.badge-state {
-  width: max-content;
-  height: auto;
-  display: inline-flex;
-  padding: 2px 12px;
-  margin-left: 3px;
+      @apply bg-background-primary;
+      border-radius: 30px;
 
-  border: none;
-  border-radius: 20px;
-}
+      transition: transform 0.35s ease-in-out;
+      transform: scale(0.95);
 
-.badge-state {
-  background: #b72b74;
-}
+      .layer {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        z-index: 0;
+        background: rgba($color: #000000, $alpha: 0.22);
+        transition: background 0.35s ease-in-out;
+      }
 
-.badge-state.completed {
-  background: #5aa70c;
+      .banner {
+        height: calc(#{$height-card} * 0.4);
+        width: 100%;
+
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: top;
+      }
+
+      .content {
+        height: calc(#{$height-card} * 0.6);
+        padding: 20px;
+        position: relative;
+        user-select: none;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+
+        h2 {
+          font-size: 24px;
+          font-weight: 700;
+        }
+
+        p,
+        li,
+        span {
+          font-size: 15px;
+        }
+      }
+
+      &:hover {
+        transform: scale(1);
+
+        .layer {
+          background: rgba($color: #000000, $alpha: 0);
+        }
+      }
+
+      .badge-state {
+        width: max-content;
+        height: auto;
+        display: inline-flex;
+        padding: 2px 12px;
+        margin-left: 3px;
+
+        background: #b72b74;
+        border: none;
+        border-radius: 20px;
+
+        &.completed {
+          background: #5aa70c;
+        }
+      }
+    }
+  }
+
+  /* RESPONSIVE */
+  @media (max-width: 540px) {
+    .container {
+      width: 100%;
+      margin: 0;
+      padding: 10px 5px;
+
+      grid-template-columns: 1fr;
+      grid-template-rows: auto;
+
+      .card {
+        .layer {
+          display: none;
+        }
+
+        .content {
+          h2 {
+            font-size: 20px;
+          }
+
+          p,
+          li,
+          span {
+            font-size: 14px;
+          }
+        }
+      }
+    }
+  }
 }
 </style>
